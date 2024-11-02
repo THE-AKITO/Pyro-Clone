7abe776517094e562d731ec6c718576cd5b3e4cb
AKITO <dipeshkoli86@gmail.com>
2024-11-02 17:12:33 +0530

    Update __main__.py

--- Clone/__main__.py
+++ Clone/__main__.py
@@ -1,4 +1,14 @@
-from __init__.py import bot
+from pyrogram import Client
+
+
+
+# Set up bot configurations
+API_ID = 29764570
+API_HASH = "7676192e491329a1b859b2184e5d7823"
+TOKEN = "7402282033:AAFnpVWwM3kCW1_HtZqRdGspatFl6_ZbLgE"
+
+#PYROGRAM BOT CLIENT
+bot = Client(name="CloneBot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Clone.Examples"))
       
 if __name__ == "__main__":
       bot.run()
